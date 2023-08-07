4f3c842f477db3f8196460e9534b5db7c2f9f2a7
kayos@tcp.direct <kayos@tcp.direct>
2023-08-07 15:51:18 -0700

    Fix: remove gotrace

--- internal/scaler/scaler_test.go
+++ internal/scaler/scaler_test.go
@@ -4,12 +4,7 @@ import (
 	"testing"
 
 	"github.com/panjf2000/ants/v2"
-	"github.com/ysmood/gotrace"
 )
-
-func checkLeak(t *testing.T) {
-	gotrace.CheckLeak(t, 0)
-}
 
 var dummyPool *ants.Pool
 
@@ -25,7 +20,6 @@ func init() {
 // With that being said, this test could be much more exhaustive.
 // For now, this can serve as a sanity check.
 func TestNewAutoScaler(t *testing.T) {
-	checkLeak(t)
 	// debugSwitch = true
 	as := NewAutoScaler(5, 50, 10)
 	if as.IsOn() {
